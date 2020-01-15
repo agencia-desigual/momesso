46aabf7dd95424f02952b051f19c39132804172f
Edilson Pereira <edilson.pereira.iami@gmail.com>
2020-01-15 09:44:24 -0300

    Ajustes SEO

--- app/views/site/includes/footer.php
+++ app/views/site/includes/footer.php
@@ -1,21 +1,21 @@
 <footer>
     <div class="container">
         <div style="padding: 50px 0px;" class="row">
-            <div class="col-md-4">
-                <img src="<?= BASE_URL; ?>arquivos/assets/img/momesso_logo_rodape.png">
+            <div class="col-md-4 logo-rodape">
+                <img class="" src="<?= BASE_URL; ?>arquivos/assets/img/momesso_logo_rodape.png">
             </div>
-            <div class="col-md-8 text-right">
+            <div class="col-md-8 text-right logo-rodape">
                 <a href="<?= BASE_URL; ?>" class="footer-itens">HOME</a>
                 <a href="<?= BASE_URL; ?>" class="footer-itens">EMPRESA</a>
                 <a href="<?= BASE_URL; ?>" class="footer-itens">PRODUTOS</a>
                 <a href="<?= BASE_URL; ?>contato" class="footer-itens">CONTATO</a>
-                <a href="#" class="menu-itens" style="padding: 0px 3px; vertical-align: super"><img width="35px" src="<?= BASE_URL; ?>arquivos/assets/img/icone-face-rodape.png"></a>
-                <a href="#" class="menu-itens" style="padding: 0px 3px; vertical-align: super"><img width="35px" src="<?= BASE_URL; ?>arquivos/assets/img/icone-youtube-rodape.png"></a>
+                <a href="#" class="menu-itens rede-social-rodape" style="padding: 0px 3px; vertical-align: super"><img width="35px" src="<?= BASE_URL; ?>arquivos/assets/img/icone-face-rodape.png"></a>
+                <a href="#" class="menu-itens rede-social-rodape-yt" style="padding: 0px 3px; vertical-align: super"><img width="35px" src="<?= BASE_URL; ?>arquivos/assets/img/icone-youtube-rodape.png"></a>
             </div>
         </div>
         <hr style="border-top: 1px solid #5b5b5b;">
         <div style="padding-bottom: 30px" class="row">
-            <div class="col-md-12">
+            <div class="col-md-12 endereco-rodape">
                 <p style="color: #5b5b5b; margin-bottom: -5px">Av. João Cernack, 999 - CEP.:16200-054 - Birigui/SP   Tel.: + 55 18 3642-2460</p>
                 <p style="color: #5b5b5b">Momesso® - <?php echo date('Y'); ?> - Todos os direitos reservados</p>
             </div>
